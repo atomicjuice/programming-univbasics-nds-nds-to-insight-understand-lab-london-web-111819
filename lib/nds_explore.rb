605c868dbf46ea93e44a3e8c26ea5666db4a6b8f
keemo <keemowatson1@gmail.com>
2019-11-14 23:36:00 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,10 +11,10 @@ def print_first_directors_movie_titles
   # dir = []
   #  dir << (pp directors_database[0][:movies][1][:title])
   # puts dir
-  puts directors_database.count
+  puts directors_database[0][:movies].count
   # row = 0
   # dir = []
-  # while row < directors_database.count
+  # while row < directors_database[0][:movies][0].count
   # dir << directors_database[0][:movies][row][:title]
   #   row += 1
   # end
