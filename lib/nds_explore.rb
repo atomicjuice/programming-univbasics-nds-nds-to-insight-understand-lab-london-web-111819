975d5701ab1e9bf4a4243b71850deb6320bcc9ad
keemo <keemowatson1@gmail.com>
2019-11-14 23:04:49 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,7 +13,7 @@ def print_first_directors_movie_titles
   dir = []
   while row < directors_database.count
     dir << directors_database[0][:movies][0][:title]
-    require.pry
+    require pry
     row += 1
   end
   dir
