041f9a24c47611db8db6818fec2c4b365998eb2b
keemo <keemowatson1@gmail.com>
2019-11-14 23:26:25 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,17 +8,15 @@ def pretty_print_nds(nds)
 end
 require "pry"
 def print_first_directors_movie_titles
-  dir = []
-   dir << (pp directors_database[0][:movies][1][:title])
-  puts dir
-  # row = 0
   # dir = []
-  # while row < directors_database.count
-  # puts  dir << directors_database[0][:movies][0][:title]
-  #   row += 1
-  # end
+  #  dir << (pp directors_database[0][:movies][1][:title])
+  # puts dir
+  row = 0
+  dir = []
+  while row < directors_database.count
+  puts  dir << directors_database[0][:movies][row][:title]
+    row += 1
+  end
   # require.pry
-
-  # dir
-
+  dir
 end
