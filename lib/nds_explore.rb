16360e47b7ee47d9776020ea6b22db0f763e627b
keemo <keemowatson1@gmail.com>
2019-11-14 23:23:58 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,7 +9,7 @@ end
 require "pry"
 def print_first_directors_movie_titles
   dir = []
-   dir << (pp directors_database[0][:movies][0][:title])
+   dir << (pp directors_database[0][:movies][1][:title])
   puts dir
   # row = 0
   # dir = []
